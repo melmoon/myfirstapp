6cb3838f0687c697ed3b163f218bfbada3625a71
melmoon <>
2014-05-14 09:17:18 +0700

    my first project

--- src/myfirstapp/Myfirstapp.java
+++ src/myfirstapp/Myfirstapp.java
@@ -18,6 +18,7 @@ public class Myfirstapp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("my name is Phakamat Jadprem");
     }
     
 }
